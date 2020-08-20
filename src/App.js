042cd8ac83b989b1b5d2ae51a4c178707d47ead8
abdul23lm <abdul23lm@gmail.com>
2020-08-21 03:13:37 +0700

    Styling navbar header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "assets/scss/style.scss";
+import LandingPage from "elements/pages/LandingPage";
 function App() {
-  return <div className="App">BooKost Begin!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
 }
 export default App;
