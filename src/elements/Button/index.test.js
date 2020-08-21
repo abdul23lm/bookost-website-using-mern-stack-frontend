0fd842e315c7f8f65dbf82e7f5cc1353f4cca86c
abdul23lm <abdul23lm@gmail.com>
2020-08-22 03:08:12 +0700

    Fix index.test.js

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -4,16 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Button from "./index";
 
 test("Should not allowed click button if isDisabled is present", () => {
-  const { container } = render(<Button isDisabled></Button>);
+  const { container } = render(<Button isDisabled> </Button>);
 
   expect(container.querySelector("span.disabled")).toBeInTheDocument();
 });
 
 test("Should render loading/spinner", () => {
-  const { container, getByText } = render(<Button isLoading></Button>);
+  const { container, getByText } = render(<Button isLoading> </Button>);
 
   expect(getByText(/loading/i)).toBeInTheDocument();
-  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+  expect(container.querySelector("span.disabled")).not.toBeInTheDocument();
 });
 
 test("Should render <a> tage", () => {
@@ -29,5 +29,5 @@ test("Should render <Link> component", () => {
     </Router>
   );
 
-  expect(container.querySelector("a")).toBeInTheDocument();
+  expect(container.querySelector("a")).not.toBeInTheDocument();
 });
