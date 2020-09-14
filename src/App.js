c30e5a01a0d6418cf0bfcf0c39c93316e4f2375b
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-09-14 23:16:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
+import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 
 function App() {
