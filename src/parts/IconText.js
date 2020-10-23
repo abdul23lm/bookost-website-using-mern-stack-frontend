bfa039797760ef404026743eeb13a0564ac17392
abdul23lm <abdul23lm@gmail.com>
2020-10-24 00:28:18 +0700

    Fix active status on navigation hover

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -4,7 +4,7 @@ import Button from "elements/Button";
 
 export default function IconText() {
   return (
-    <Button classNam="brand-text-icon" href="" type="link">
+    <Button className="brand-text-icon" href="" type="link">
       Book<span className="text-gray-900">ost</span>
     </Button>
   );
