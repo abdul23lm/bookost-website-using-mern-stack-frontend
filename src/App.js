e11717d5dd0f31dfb211eadeecb501d9a4bb78c5
abdul23lm <abdul23lm@gmail.com>
2020-10-30 05:05:17 +0700

    Add isCentered header on checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 import ExampleInputNumber from "pages/ExampleInputNumber";
 import ExampleInputDate from "pages/ExampleInputDate";
 import ExampleBreadcrumb from "pages/ExampleBreadcrumb";
@@ -14,6 +15,7 @@ export default function App() {
       <Router>
           <Route exact path="/" component={LandingPage}/>
           <Route exact path="/detail-kostan/:id" component={DetailsPage}/>
+          <Route exact path="/checkout" component={Checkout}/>
           <Route path="/example-input-number" component={ExampleInputNumber} />
           <Route path="/example-input-date" component={ExampleInputDate} />
           <Route path="/example-breadcrumb" component={ExampleBreadcrumb} />
