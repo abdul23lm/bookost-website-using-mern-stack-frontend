62e6642b976b41b95978480a62fa5a0dfd70fd0b
abdul23lm <abdul23lm@gmail.com>
2020-10-30 10:17:41 +0700

    Fix unused function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
