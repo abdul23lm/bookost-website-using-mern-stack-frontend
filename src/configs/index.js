988e683d88b6c92e9d84a64ff540d5184fb7f020
abdul23lm <abdul23lm@gmail.com>
2020-11-04 04:36:20 +0700

    Setup errorResponseHandler

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// import errorResponseHandler from "./errorResponseHandler";
+import errorResponseHandler from "./errorResponseHandler";
 
 const instance = axios.create({
   baseURL: `https://cors-anywhere.herokuapp.com/${process.env.REACT_APP_HOST}/api/v1/member`,
 });
 
-// instance.interceptors.response.use(
-//   (response) => response,
-//   errorResponseHandler
-// );
+instance.interceptors.response.use(
+  (response) => response,
+  errorResponseHandler
+);
 
 export default instance;
